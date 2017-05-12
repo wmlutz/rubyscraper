212a431998e5922662261d04e1cec35700934d2f
wmlutz <william.meany.lutz@gmail.com>
2017-05-12 16:11:45 -0400

    Works except for date issue

--- googledrive.rb
+++ googledrive.rb
@@ -1,10 +1,9 @@
-# Ruby to Google Drive connecting
-# Plan to print out X,Y,Z into a google drive sheet.
-
 require 'google/apis/sheets_v4'
 require 'googleauth'
 require 'googleauth/stores/file_token_store'
 require 'json'
+require 'rubygems'
+require 'mechanize'
 
 require 'fileutils'
 
@@ -48,14 +47,14 @@ service = Google::Apis::SheetsV4::SheetsService.new
 service.client_options.application_name = APPLICATION_NAME
 service.authorization = authorize
 
-def to_json(options = {})
-  JSON.pretty_generate(self, options)
-end
+# def to_json(options = {})
+#   JSON.pretty_generate(self, options)
+# end
 # The spreadsheet to request.
 spreadsheet_id = '1OchgafoPg22jQgh3WU2v_9Rfy3YIVPls7s135tpCh5I'
 
 # The ranges to retrieve from the spreadsheet.
-range = ['Sheet1!A1:B1']
+range = ['Sheet1!A1:D1']
 
 # include_grid_data = true
 # #response = service.get_spreadsheet(spreadsheet_id, ranges: ranges, include_grid_data: include_grid_data)
@@ -68,17 +67,52 @@ value_input_option = 'RAW'
 # How the input data should be inserted.
 insert_data_option = 'INSERT_ROWS'
 
+def getPayload
+  a = Mechanize.new { |agent|
+    agent.user_agent_alias = 'Mac Safari'
+  }
+
+  page = a.get('https://www.crunchbase.com/funding-rounds')
+  resultsarray = []
+
+  # Get the information we need from the page
+  puts "before loop"
+  funddate = Time.now.strftime("%m/%d/%Y")
+  page.search('.info-block').map do |c|					# block that holds the company name
+		name = c.search('.name').text.strip			# the company name itself
+		fundamt = c.search('.funding_amount').text.strip		# funding amount
+		# fundamt = fundamt[0,fundamt.length-2]
+		fundround = c.search('.funding-type').text.strip # funding round
+		fundclean = fundamt[0,(fundamt.length-2)]
+
+		# puts name
+		# puts fundclean
+		# puts fundround
+		# puts funddate
+		# puts "--------"
+
+		unless resultsarray.include? [name, fundclean, fundround, funddate]	# checks if the entry is already in array
+			resultsarray << [name, fundclean, fundround, funddate]
+		end
+  end
+  puts "end of loop"
+
+  return resultsarray
+end
+
+data = getPayload
 # Assign values to desired members of `request_body`:
 payload = Google::Apis::SheetsV4::ValueRange.new
 payload = {
-  "range": "Sheet1!A1:B1",
+  "range": "Sheet1!A1:D1",
   "majorDimension": "ROWS",
-  "values": [
-    ["Max Hart", "Meat Stick"],
-    ["Evan Walkker","Spazoid"],
-  ],
+  "values": data,
 }
 
+puts payload.to_s
+
+
+
 ## ------------------ Turn on when ready to add values
-#response = service.append_spreadsheet_value(spreadsheet_id, range, payload, value_input_option: value_input_option, insert_data_option: insert_data_option)
-#puts response.to_json
+response = service.append_spreadsheet_value(spreadsheet_id, range, payload, value_input_option: value_input_option, insert_data_option: insert_data_option)
+puts response.to_json
